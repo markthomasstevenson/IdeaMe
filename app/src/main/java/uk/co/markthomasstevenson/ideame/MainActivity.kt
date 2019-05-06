1f4dd57f89f0f6c0afca78207ab5a1b6ca7118bc
Mark Stevenson <ghostt.tw@gmail.com>
2019-05-06 12:10:21 +0100

    Removed default Action Bar Tidied up Navigation - linked with new Toolbar Added Back link to Navigation

--- app/src/main/java/uk/co/markthomasstevenson/ideame/MainActivity.kt
+++ app/src/main/java/uk/co/markthomasstevenson/ideame/MainActivity.kt
@@ -2,7 +2,13 @@ package uk.co.markthomasstevenson.ideame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
+import android.widget.Toast
+import android.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -11,15 +17,27 @@ class MainActivity : AppCompatActivity() {
     private var plusToTick: AnimatedVectorDrawableCompat? = null
     private var tickToPlus: AnimatedVectorDrawableCompat? = null
     private var showingPlus = true
+    lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val navController = findNavController(nav_host_fragment)
+        val appBarConfiguration =  AppBarConfiguration.Builder(navController.graph).build()
+        setupWithNavController(toolbar, navController, appBarConfiguration)
+
         plusToTick = AnimatedVectorDrawableCompat.create(baseContext, R.drawable.plustotick)
         tickToPlus = AnimatedVectorDrawableCompat.create(baseContext, R.drawable.ticktoplus)
         fab.setImageDrawable(plusToTick)
-        fab.setOnClickListener{ morph() }
+        fab.setOnClickListener{
+            if(showingPlus) {
+                navController.navigate(R.id.action_ideaListFragment_to_createIdeaFragment)
+            } else {
+                navController.navigate(R.id.action_createIdeaFragment_to_ideaListFragment)
+            }
+            morph()
+        }
         showingPlus = true
     }
 
