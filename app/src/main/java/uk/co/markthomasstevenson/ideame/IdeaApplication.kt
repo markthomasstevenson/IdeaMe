a858dc486d51f01e058a802cc3a1e0c3aa7aa622
Mark Stevenson <ghostt.tw@gmail.com>
2019-05-27 13:10:36 +0100

    Added empty Migrations class for future changes

--- app/src/main/java/uk/co/markthomasstevenson/ideame/IdeaApplication.kt
+++ app/src/main/java/uk/co/markthomasstevenson/ideame/IdeaApplication.kt
@@ -4,12 +4,17 @@ import android.app.Application
 import androidx.multidex.MultiDexApplication
 import io.realm.Realm
 import io.realm.RealmConfiguration
+import uk.co.markthomasstevenson.ideame.data.Migrations
 
 class IdeaApplication : MultiDexApplication() {
     override fun onCreate() {
         super.onCreate()
         Realm.init(this)
-        val config = RealmConfiguration.Builder().modules(IdeaDBSchema()).name("idea-me.realm").deleteRealmIfMigrationNeeded().build()
+        val config = RealmConfiguration.Builder().modules(IdeaDBSchema())
+                .name("idea-me.realm")
+                .schemaVersion(0)
+                .migration(Migrations())
+                .build()
         Realm.setDefaultConfiguration(config)
     }
 }
